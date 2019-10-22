ce2cd18fa868cdbac985c66fe96f3cdc6b52e395
Danny Rehn <danny.rehn@gmail.com>
2019-10-22 15:44:23 -0700

    Update calculations folder

--- lithiation/calculations/plotcharge-diff.py
+++ lithiation/calculations/plotcharge-diff.py
@@ -65,8 +65,8 @@ def getchargearray(HL,folder):
 
         
 if __name__ == '__main__':
-    folders = ['3-electron-relax-bilayer-li', '4-electron-relax-bilayer']
-    HLINES = [21,20]
+    folders = ['3-Li-bilayer-0.5', '4-Li-bilayer-0.45', '5-Li-bilayer-0.4', '6-bilayer-only']
+    HLINES = [21,21,21,20]
     
     if len(folders) != len(HLINES):
         print("Error: folders and HLINES are not the same size")
@@ -79,32 +79,47 @@ if __name__ == '__main__':
         chargeresults[:,i] = getchargearray(HLINES[i], folders[i])
             
 
-    if len(folders) == 2:
-        diff = chargeresults[:,0] - chargeresults[:,1]
+    if len(folders) == 4:
+        diff1 = chargeresults[:,0] - chargeresults[:,3]
+        diff2 = chargeresults[:,1] - chargeresults[:,3]
+        diff3 = chargeresults[:,2] - chargeresults[:,3]
 
-    totalcharge = sum(diff)
+    totalcharge1 = sum(diff1)
+    totalcharge2 = sum(diff2)
+    totalcharge3 = sum(diff3)
+
     nelectrons = 1
     figure(figsize=(9,15))
-    plot(diff/totalcharge*nelectrons,arange(nz)/nz*35)
+    plot(diff1/totalcharge1*nelectrons,arange(nz)/nz*35,color="#B1D1D0")
+    plot(diff2/totalcharge2*nelectrons,arange(nz)/nz*35,color="#69A4A0")
+    plot(diff3/totalcharge3*nelectrons,arange(nz)/nz*35,color="#517878")
     ylim(0,35)
     xlim(-0.015, 0.1)
-    xlabel('Charge fraction at location $z$',fontsize=34)
-    ylabel('$z$ ($\AA$)')
+    xlabel('$q(z)$')
+    ylabel('$z$ ($\mathrm{\AA}$)')
     tight_layout()
     savefig('charge_vs_z.png', bbox_inches='tight')
+    #show()
 
+    chargezsum1 = zeros(nz)
+    chargezsum2 = zeros(nz)
+    chargezsum3 = zeros(nz)
 
-    chargezsum = zeros(nz)
     for i in range(nz):
-        chargezsum[i] = sum(diff[:i])
-
+        chargezsum1[i] = sum(diff1[:i])
+        chargezsum2[i] = sum(diff2[:i])
+        chargezsum3[i] = sum(diff3[:i])
+        
     nelectrons=1
     figure(figsize=(9,15))
-    plot(chargezsum/totalcharge*nelectrons,arange(nz)/nz*35)
-    axvline(1,color='g')
-    ylabel('$z$ ($\AA$)')
-    xlabel('Integrated \# electrons at location $z$',fontsize=34)
+    plot(chargezsum1/totalcharge1*nelectrons,arange(nz)/nz*35,color="#B1D1D0")
+    plot(chargezsum2/totalcharge2*nelectrons,arange(nz)/nz*35,color="#69A4A0")
+    plot(chargezsum3/totalcharge3*nelectrons,arange(nz)/nz*35,color="#517878")
+    #axvline(1,color='g')
+    ylabel('$z$ ($\mathrm{\AA}$)')
+    xlabel('$N(z)$')
     ylim(0,35)
     xlim(-0.1,1.1)
     tight_layout()
     savefig("sumcharge_vs_z.png", bbox_inches='tight')        
+    #show()
---
+++ lithiation/calculations/readme.md
@@ -0,0 +1,23 @@
+# Calculations of lithiation of bilayer
+
+These calculations use the SCAN + rVV10 exchange-correlation
+functional. There are several steps in these calculations, each of
+which is performed in a separate folder, summarized here:
+
+- `1-relax-bilayer/`: perform variable-cell relaxation on the bilayer
+  (IBRION=2; ISIF=3) to allow the lattice constants and atom positions
+  to change.
+- `2-relax-bilayer-fixed/`: take the a, b lattice constants from (1) and
+  use these in a new relaxation in which the cell is not allowed to
+  change (IBRION=2; ISIF=2). The lattice constants (a,b) used are not
+  taken exactly from `1-relax-bilayer` because we want to enforce the
+  orthorhombic cell exactly.
+- `3-Li-bilayer-0.5/`: Using the relaxed bilayer from (2), add a
+  lithium atom at [0.5, 0.5, 0.5] in the cell and perform an electronic
+  relaxation (fixed ions, fixed lattice: IBRION=-1).  Also switch to
+  tetrahedral integration for higher accuracy calculations.
+- `4-Li-bilayer-0.45/`: Same as 3, but with the Li atom at [0.5, 0.5, 0.45]
+- `5-Li-bilayer-0.4/`: Same as 3, but with the Li atom at [0.5, 0.5, 0.4]
+- `6-bilayer-only/`: Same as 3, but without the Li atom (needed so
+  that we can subtract the charge density from folders 3,4,5.
+
